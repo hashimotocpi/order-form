7645e31934ec74b27768d3c249f065b63e48d676
Hashimoto <hashimoto.cpi@gmail.com>
2026-05-30 10:00:00 +0900

    update

--- api/gas.js
+++ api/gas.js
@@ -1,20 +1,3 @@
-export default async function handler(req, res) {
-    try {
-      const response = await fetch(
-        "https://script.google.com/macros/s/AKfycbyJtZsABuzOZGN0mOqZQ-uhuT2NGwAKGJkVlxvM-_jPIKX60TgH0nqa1gEv3edWA9Or_A/exec",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(req.body),
-        }
-      );
-  
-      const data = await response.json();
-  
-      res.status(200).json(data);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
+export default function handler(req, res) {
+    res.status(200).json({ ok: true });
   }

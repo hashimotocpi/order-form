1531748c6513611d8c8cf5972b763c4277ef820d
Hashimoto <hashimoto.cpi@gmail.com>
2026-05-30 09:33:09 +0900

    update

--- api/gas.js
+++ api/gas.js
@@ -1,12 +1,15 @@
 export default async function handler(req, res) {
     try {
-      const response = await fetch(process.env.GAS_URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(req.body),
-      });
+      const response = await fetch(
+        "https://script.google.com/macros/s/AKfycbyJtZsABuzOZGN0mOqZQ-uhuT2NGwAKGJkVlxvM-_jPIKX60TgH0nqa1gEv3edWA9Or_A/exec",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(req.body),
+        }
+      );
   
       const data = await response.json();
   

ad8d60b85acf762f410125ca03a92b51e21cde20
Hashimoto <hashimoto.cpi@gmail.com>
2026-06-06 14:24:54 +0900

    fix inquiry get

--- api/inquiry.js
+++ api/inquiry.js
@@ -4,13 +4,18 @@ export default async function handler(req, res) {
   try {
     const { inquiryId } = req.query;
 
-    const data = await callGAS({
-      type: "getInquiry",
-      inquiryId,
-    });
+    const url =
+      "https://script.google.com/macros/s/AKfycbw_H4qpReLYRlcVQFJADRcJEhlurUYNUnnc-Toi0gLaezAo38J1CDng7LeM1s7dqajY/exec" +
+      `?type=getInquiry&inquiryId=${encodeURIComponent(inquiryId)}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
 
     return res.status(200).json(data);
+
   } catch (err) {
-    return res.status(500).json({ error: err.toString() });
+    return res.status(500).json({
+      error: err.toString()
+    });
   }
 }

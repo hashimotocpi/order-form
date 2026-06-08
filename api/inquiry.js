afe6630a35cb0ca972403f594bd38d19913b1696
Hashimoto <hashimoto.cpi@gmail.com>
2026-06-08 19:52:59 +0900

    debug lib path

--- api/inquiry.js
+++ api/inquiry.js
@@ -2,32 +2,31 @@ export default async function handler(req, res) {
   try {
     const { inquiryId } = req.query;
 
+    if (!inquiryId) {
+      return res.status(400).json({
+        success: false,
+        error: "inquiryId is required"
+      });
+    }
+
     const url =
       "https://script.google.com/macros/s/AKfycbw_H4qpReLYRlcVQFJADRcJEhlurUYNUnnc-Toi0gLaezAo38J1CDng7LeM1s7dqajY/exec" +
       `?type=getInquiry&inquiryId=${encodeURIComponent(inquiryId)}`;
-
-    console.log("URL:", url);
 
     const response = await fetch(url);
 
     const text = await response.text();
 
-    console.log("RAW GAS RESPONSE:");
-    console.log(text);
+    console.log("RAW GAS:", text);
 
-    return res.status(200).json({
-      success: true,
-      raw: text,
-    });
+    const data = JSON.parse(text);
+
+    return res.status(200).json(data);
 
   } catch (err) {
-
-    console.error("API ERROR:", err);
-
     return res.status(500).json({
       success: false,
       error: err.toString(),
-      stack: err.stack,
     });
   }
 }
